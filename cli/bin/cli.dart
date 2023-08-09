169b94f5a998ec31835da361f4c89f51bcab866f
hiano7972 <hiano7972@gmail.com>
2023-08-09 14:58:21 +0900

    named parameter

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,8 +1,9 @@
-import 'package:cli/cli.dart' as cli;
+// import 'package:cli/cli.dart' as cli;
 
-int cal(int n1, int n2)=>n1*n2;
+int cal({required int n1,int n2 = 1})=>n1*n2;
 
 void main(List<String> arguments) {
   // print('Hello world: ${cli.calculate()}!');
-  print('result is ${cal(5,8)}');
+  print('result is ${cal(n1 : 5,n2 : 8)}');
 }
+ 
