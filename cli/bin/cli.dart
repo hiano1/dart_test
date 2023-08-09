c2c20a8390882b507b7284b8354c0fa54bbef709
hiano7972 <hiano7972@gmail.com>
2023-08-09 16:17:06 +0900

    QQ

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -5,12 +5,28 @@ int cal({required int n1,int n2 = 1})=>n1*n2;
 //optional
 String hi(String name, int age , [String? country = 'korea'])
 => 'hi $name, you are $age years od from $country';
+//?? ?= QQ operator
+String capitalizeName(String? name) => 
+name != null ?name.toUpperCase() : 'ANON';
+
+String capitalizeName2(String? name) => 
+name?.toUpperCase() ?? 'ANON';
+
 
 void main(List<String> arguments) {
   // print('Hello world: ${cli.calculate()}!');
   //print named
   // print('result is ${cal(n1 : 5,n2 : 8)}');
   //print optional
-  var result = hi('hiano',19);
-  print(result);
+  // var result = hi('hiano',19);
+  var result = capitalizeName('hiano');
+  var result2 = capitalizeName2('hiano');
+
+  print('$result\n$result2');
+
+  //and..
+
+  String? name;
+  name ??= 'hiano';
+  print(name);
 }
