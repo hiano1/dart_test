c9f72c1597d7773736c1daad095656d2264b4e58
hiano7972 <hiano7972@gmail.com>
2023-08-09 18:08:00 +0900

    named constructor

--- cli/bin/class.dart
+++ cli/bin/class.dart
@@ -1,19 +1,41 @@
-
-class Player{
+class Player {
   final String name;
   int xp;
-  //constructor
-  Player(this.name, this.xp);
+  String team;
+  int age;
 
-  void sayHello(){
+  //constructor
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  //named constructor
+  Player.createBluePlayer({required String inputName, required int inputAge})
+      : name = inputName,
+        age = inputAge,
+        team = 'blue',
+        xp = 0;
+
+  void sayHello() {
     print("hello my name is $name");
+  }
+
+  void sayAll() {
+    print('name is $name');
+    print('age is $age');
+    print('xp is $xp');
+    print('team is $team');
   }
 }
 
-
-
 void main(List<String> args) {
-  var player = Player('hiano',1000);
+  var player = Player(name: 'hiano', xp: 1000, team: 'red', age: 12);
   player.sayHello();
-  
+  print(player.team);
+
+  var player2 = Player.createBluePlayer(inputName: 'hiano1', inputAge: 15);
+  player2.sayAll();
 }
