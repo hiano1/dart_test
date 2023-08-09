aa69af1503c77074fa7098fdfa9fe291b81d2e29
hiano7972 <hiano7972@gmail.com>
2023-08-09 15:14:49 +0900

    optionanl parameter

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,9 +1,16 @@
 // import 'package:cli/cli.dart' as cli;
 //123;
+//named
 int cal({required int n1,int n2 = 1})=>n1*n2;
+//optional
+String hi(String name, int age , [String? country = 'korea'])
+=> 'hi $name, you are $age years od from $country';
 
 void main(List<String> arguments) {
   // print('Hello world: ${cli.calculate()}!');
-  print('result is ${cal(n1 : 5,n2 : 8)}');
+  //print named
+  // print('result is ${cal(n1 : 5,n2 : 8)}');
+  //print optional
+  var result = hi('hiano',19);
+  print(result);
 }
- 
