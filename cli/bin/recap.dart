350edb5c34edcb5ae38eb1054cc3231a20518def
hiano7972 <hiano7972@gmail.com>
2023-08-10 11:45:41 +0900

    recap

---
+++ cli/bin/recap.dart
@@ -0,0 +1,39 @@
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print("hello my name is $name");
+  }
+}
+
+void main(List<String> args) {
+  var apiData = {
+    {
+      "name": "hiano",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "martin",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "jhon",
+      "team": "red",
+      "xp": 0,
+    },
+  };
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
+}
